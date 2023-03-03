62510579a8f4a8ed36dd43e8a8cb63eb03d8c757
ayequill <ayequillgh@gmail.com>
2023-03-03 02:17:11 +0000

    using chakra ui and added axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
+import { Container, extendTheme } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [searchValue, setSearchValue] = useState("");
+  const [word, setWord] = useState([]);
+
+  function getSearchValue(e) {
+    const target = e.target;
+    if (target.value.length > 0){
+      setSearchValue(target.value);
+    }
+  }
+  // const {data} = axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchValue}`)
+  // .then(res => res.data)
+  // .catch(error => console.log(error))
+  
+  // console.log(data)
+
+  useEffect(()=> {
+    getWord
+  }, [searchValue])
+  
+  console.log(word)
+  
+
+  function getWord(e) {
+    if (e.key === "Enter" && searchValue.length > 0) {
+      axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchValue}`)
+      .then(res => setWord(res.data) )
+      .catch(error => console.log(error))
+    }
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Container
+      w={[480, 600, 1400]}
+      maxW="100%"
+      border="1px solid black"
+      minH={"100vh"}
+    >
+      <Header />
+      <SearchBar
+        getValue={getSearchValue}
+        getWord={getWord}
+        searchWord={searchValue}
+      />
+    </Container>
+  );
 }
 
-export default App
+export default App;
