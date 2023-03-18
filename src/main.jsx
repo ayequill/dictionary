37a6cf5b4ba27b1f6d82a42cd6af7958259f8588
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-03-18 03:47:56 +0000

    defined global styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import App from './App'
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      // styles for the `body`
+      body: {
+        bg: 'gray.50',
+        color: 'blackAlpha.800',
+      },
+      // styles for the `a`
+      a: {
+        color: 'teal.500',
+        _hover: {
+          textDecoration: 'underline',
+        },
+      },
+    },
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider resetCSS='true'>
+    <ChakraProvider resetCSS='true' theme={theme}>
     <App />
     </ChakraProvider>
   </React.StrictMode>,
